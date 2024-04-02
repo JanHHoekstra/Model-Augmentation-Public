d242bd2116a210c4f0f1f8134f0394a9d06a3913
Mixxxxx358 <78261802+Mixxxxx358@users.noreply.github.com>
2024-04-02 15:54:07 +0200

    Update setup.py
    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup
 if __name__ == '__main__':
-    setup()
+    setup(name = 'ModelAugmentation',
+      author = 'Jan Hoekstra',
+      author_email = 'j.h.hoekstra@tue.nl',
+      license = 'BSD 3-Clause License',
+      python_requires = '>=3.9'
+    )
